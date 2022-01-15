cd5e7c32301f06c43891e0d587cb6d2257f48742
Sophia Chan <ianweng.chan@gmail.com>
2022-01-14 20:24:10 -0500

    New algorithm, update EMwwdQ2
    Update EMwwdQ2 based on the new algorithm; change in the prior for ww (now a standard deviation that may not be 1)

--- src/EMwwdQ2.cpp
+++ src/EMwwdQ2.cpp
@@ -12,10 +12,11 @@ using namespace Rcpp;
 //
 
 // [[Rcpp::export]]
-SEXP EMwwdQ2(SEXP p, SEXP betal, SEXP tl) {
+SEXP EMwwdQ2(SEXP p, SEXP betal, SEXP tl, double sigma) {
   NumericMatrix pp(p); // N by g matrix
   NumericVector bbetal(betal); // length g
   NumericMatrix ttl(tl); // N by Sl matrix
+  double sd(sigma);
   NumericVector prodsq(pp.nrow()); // length N, pp*beta
   NumericVector prod(pp.nrow()); // length N
   NumericMatrix temp(ttl.nrow(), ttl.ncol()); // N by Sl
@@ -32,7 +33,7 @@ SEXP EMwwdQ2(SEXP p, SEXP betal, SEXP tl) {
     for(int s=0; s<result.ncol(); s++){
       // s=1,...,Sl clusters
       result(j,s) = sum(temp(_,j)*ttl(_,s));
-      if(s==j) result(j,s) = result(j,s) -1.0; // minus identity
+      if(s==j) result(j,s) = result(j,s) -1.0/(sd*sd); // minus (identity divided by variance)
     }
   }
   return result;
